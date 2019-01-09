eb7f81c3474064eeadc195b8c7f7241f3ca4e7e5
Nestor Torres <ntorres1194@gmail.com>
2019-01-08 19:29:56 -0500

    feat: initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 
+import "./App.scss";
+
+// asd
+/** asdasd */
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
