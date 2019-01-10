26ea2debf374ebd5ce479b39eed0b543300f3fb2
Nestor Torres <ntorres1194@gmail.com>
2019-01-10 01:22:23 -0500

    Updates

---
+++ precache-manifest.0c77e8a4ad56c1bb36dd8c21c4899cdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "ce9d8f6b208b8d5013a0",
+    "url": "/react-test/static/js/main.ce9d8f6b.chunk.js"
+  },
+  {
+    "revision": "f9e99caa4f74f4bb05f9",
+    "url": "/react-test/static/js/1.f9e99caa.chunk.js"
+  },
+  {
+    "revision": "ce9d8f6b208b8d5013a0",
+    "url": "/react-test/static/css/main.3b01f890.chunk.css"
+  },
+  {
+    "revision": "f9e99caa4f74f4bb05f9",
+    "url": "/react-test/static/css/1.a56a75b5.chunk.css"
+  },
+  {
+    "revision": "3fd15922f96c166a16332f822d8f9c48",
+    "url": "/react-test/index.html"
+  }
+];
